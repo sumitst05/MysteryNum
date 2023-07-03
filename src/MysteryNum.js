d2f6f2c694811e59c89d24e76558c30ad7922084
sumitst05 <sumitst05@gmail.com>
2023-07-03 13:30:44 +0530

    Fixes and Updates

--- src/MysteryNum.js
+++ src/MysteryNum.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   var sparkleContainer = document.querySelector('.sparkle-container')
-
   var maxSparkles = 50 // Maximum number of sparkles in the game area
 
   function createSparkle() {
@@ -22,17 +21,27 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(createSparkle, 200)
 
   let isPlayerTurn = true
+  let isGameEnded = false
+
   function isGameOver(num, playerGuess, computerGuess) {
-    if (num === playerGuess) return 0
-    else if (num === computerGuess) return 1
-    else return -1
+    if (num === playerGuess) {
+      return 0
+    } else if (num === computerGuess) {
+      return 1
+    } else {
+      return -1
+    }
   }
 
   var num = Math.floor(Math.random() * 10) + 1 // Generate a random number between 1 and 10
 
   function checkGuess(guess, num) {
-    if (isPlayerTurn) var output = document.querySelector('.player-output')
-    else var output = document.querySelector('.computer-output')
+    var output
+    if (isPlayerTurn) {
+      output = document.querySelector('.player-output')
+    } else {
+      output = document.querySelector('.computer-output')
+    }
     if (guess > num) {
       output.innerText = 'Too High!'
     } else if (guess < num) {
@@ -45,9 +54,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var pDisplay = document.querySelector('.player-display') // Get player display element
   var numpadButtons = document.querySelectorAll('.numpad div')
 
-  function playerTurn() {
+  async function playerTurn() {
     return new Promise((resolve) => {
       function handleButtonClick() {
+        if (isGameEnded) {
+          return // Exit the function if the game has ended
+        }
         pDisplay.textContent = this.textContent // Update display content with the pressed number
         var playerGuess = parseInt(this.textContent)
         checkGuess(playerGuess, num) // Call checkGuess with the playerGuess value and target number
@@ -61,7 +73,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   var exception = 0
-  function computerTurn(mi, ma, prevPlayerGuess) {
+
+  async function computerTurn(mi, ma, prevPlayerGuess) {
     return new Promise((resolve) => {
       var computerGuess = 0
 
@@ -72,17 +85,11 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (prevPlayerGuess === ma) {
           computerGuess = mi
         } else {
-          if (exception & 1) {
-            computerGuess = mi
-          } else {
-            computerGuess = ma
-          }
+          computerGuess = exception & 1 ? mi : ma
         }
       } else {
         computerGuess = Math.floor((mi + ma) / 2)
       }
-
-      console.log(exception)
 
       var cDisplay = document.querySelector('.computer-display') // Get computer display element
       cDisplay.textContent = computerGuess
@@ -121,10 +128,12 @@ document.addEventListener('DOMContentLoaded', function () {
       } else {
         var range = adjustRange(playerGuess, num)
         computerGuess = await computerTurn(range[0], range[1], prevPlayerGuess)
-        console.log(range[0], range[1])
         range = adjustRange(computerGuess, num)
         isPlayerTurn = true
       }
+    }
+    if (isGameOver(num, playerGuess, computerGuess) !== -1) {
+      isGameEnded = true
     }
   }
 
